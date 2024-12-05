27b4c9693cb7b5c32f0d703b147d83da1c625a15
Jean Carlos Ortiz <105516848+jeanortiz02@users.noreply.github.com>
2024-12-05 15:44:28 -0400

    10 videos completed

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -9,7 +9,26 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    // console.log('Desde el cliente: %s', data);
+
+    // TODO: Enviar la data al cliente de regreso
+    // ws.send(data.toString().toUpperCase());
+
+    // * Todos incluyente 
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(data, { binary: false });
+    //   }
+    // })
+
+
+    // * Todos excluyente 
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: false });
+      }
+    });
+
   });
 
   ws.send('Hola desde el servidor');
